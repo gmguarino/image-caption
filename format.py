ede00851e22f18a6b8ce6fb0fb1bce9b56f483e0
Giuseppe Marcello Guarino <gmguarino1@gmail.com>
2021-10-20 17:29:53 +0200

    setup input format + cache

---
+++ format.py
@@ -0,0 +1,7 @@
+import fiftyone as fo
+
+# Place all dir paths in .env file
+# create instance for preprocessor inception model
+# create pytorch dataset from fiftyone dataset
+# evaluate all images w/ inceptionv3
+# Cache images in data folder
