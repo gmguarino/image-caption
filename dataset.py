e72a4d615dd5f934f10379352c83ca81926fe1e1
Giuseppe Marcello Guarino <gmguarino1@gmail.com>
2021-10-29 13:09:57 +0200

    minor changes

--- dataset.py
+++ dataset.py
@@ -13,34 +13,38 @@ load_dotenv("PATHS.env")
 
 COCO_PATH = os.environ.get("COCO_PATH")
 
+
 class COCOCaptionDataset(Dataset):
 
     def __init__(self, coco_path:str, split:str) -> None:
+        # Choose if data split
         if split not in ["train", "val"]:
             raise ValueError("Split must be 'train' | 'val'.")
         self.split = "train" if split=="train" else "validation"
+
+        # Annotations saved as list of dictionaries
         with open(os.path.join(coco_path, "raw", f"captions_{split}2017.json")) as jf:
-            captions = json.load(jf)["annotations"]
-            self.captions = pd.DataFrame.from_records(captions)
+            annotations = json.load(jf)["annotations"]
+            # Placing annotations in dataframe
+            self.annotations = pd.DataFrame.from_records(annotations)
+
         self.coco_path = coco_path
         self.image_list = os.listdir(os.path.join(COCO_PATH, self.split, "data"))
-        # Ǹeeds to be more efficient
-        self.captions["image_file"] = self.captions["image_id"].map(lambda x:f"{x:012}.jpg")
-        self.captions = self.captions.loc[self.captions.image_file.isin(self.image_list)].reset_index()
-        # self.captions = [ann for ann in self.captions if f"{ann['image_id']:012}.jpg" in self.image_list]
-        
+        self.annotations["image_file"] = self.annotations["image_id"].map(lambda x:f"{x:012}.jpg")
+        # Selection of the annotations for images present in the dataset
+        self.annotations = self.annotations.loc[self.annotations.image_file.isin(self.image_list)].reset_index()
+
         super().__init__()
 
     def __len__(self):
         return len(self.image_list)
 
     def __getitem__(self, index):
-        caption = self.captions.loc[index, "caption"]
-        image_file = self.captions.loc[index, "image_file"]
+        caption = self.annotations.loc[index, "caption"]
+        image_file = self.annotations.loc[index, "image_file"]
         filepath = os.path.join(self.coco_path, self.split, "data", image_file)
+        # return caption and PIL image
         image = Image.open(filepath)
-        print(filepath)
-
         return caption, image
 
 
